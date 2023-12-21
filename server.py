34dae5f8c5aba606605395f54b7d1f5b994bf837
EdH681 <edwardhornby07@gmail.com>
2023-12-21 01:12:41 +0000

    added basic server script

--- server.py
+++ server.py
@@ -1,3 +1,41 @@
 import socket
+import threading
+
+# =constants=
+HEADER = 64
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = "utf-8"
+DISCONNECT = "!DISCONNECT"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
 
 
+def handle_client(conn, addr):
+    print(f"{addr} connected.")
+    while True:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+
+            if msg == DISCONNECT:
+                break
+            print(f"{addr}: {msg}")
+        conn.close()
+        print(f"{addr} disconnected")
+
+
+def start():
+    server.listen()
+    print(f"Listening on ip {SERVER}")
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+
+
+print("Server starting...")
+start()
